6b3698b18606a6cc0261c872bb3f4d2f850ead23
LeifLinding <leif@scancad.dk>
2020-02-26 13:27:18 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".btn").click(function () {
+        $("nav ul").slideToggle(500);
+    })
+});
+
+$(document).ready(function () {
+    $(".navbar").click(function () {
+        $("nav ul").slideToggle(500);
+    })
+});
